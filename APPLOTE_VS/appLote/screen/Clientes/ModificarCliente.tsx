637e0e6b9ea7e4e75231f087716f97b0a2c38191
LupitaPO <guadalupepiedraolano@gmail.com>
2026-05-07 11:26:06 -0500

    Cliente
    Cambio API_URL

--- APPLOTE_VS/appLote/screen/Clientes/ModificarCliente.tsx
+++ APPLOTE_VS/appLote/screen/Clientes/ModificarCliente.tsx
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 
-const API_URL = "http://10.246.197.207:90";
+const API_URL = "http://www.tulote.somee.com/";
 
 const ModificarCliente = ({ navigation, route }) => {
   const { cliente } = route.params || {};
